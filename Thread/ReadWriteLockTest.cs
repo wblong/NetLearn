1a9c3dca92413d8f0dbc30bff7bcbd6bc70290a6
wangbaolong <wangbaolong@iseetech.com.cn>
2018-08-22 17:41:40 +0800

    read and write lock

--- Thread/ReadWriteLockTest.cs
+++ Thread/ReadWriteLockTest.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Thread
+namespace ThreadTest
 {
-    class ReadWriteLockTest
+    public class ReadWriteLockTest
     {
+
     }
 }
